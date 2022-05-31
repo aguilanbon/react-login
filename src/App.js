8633a0adf18fc3202a7385d260e8ce7465f294d4
aguilanbon <aguilanbon@gmail.com>
2022-05-31 17:33:14 +0800

    form pages login - signup router links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
+import Form from './pages/Form';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Form />
+      </BrowserRouter>
     </div>
   );
 }
