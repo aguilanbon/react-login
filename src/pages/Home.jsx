f6e746458e2f3ed1d6f44e6600e15046fef94fb4
aguilanbon <aguilanbon@gmail.com>
2022-05-31 16:16:23 +0800

    segrated components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../components/Login'
+
+function Home() {
+  return (
+    <div className='home-container'>
+        <Login />
+    </div>
+  )
+}
+
+export default Home
